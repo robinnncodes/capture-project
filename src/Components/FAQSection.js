43cc099738a6e64791927c994f26b4f68a1613d4
robin reyes <robinreyesfilms@gmail.com>
2021-04-14 22:51:17 -0700

    4. FAQ Section

---
+++ src/Components/FAQSection.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const FAQSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        {" "}
+        Any Questions <span> FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do i start?</h4>
+        <p>Lorem</p>
+        <p>Sint cupidatat ex velit mollit cillum consectetur excepteur sunt dolore proident tempor nulla adipisicing ipsum.</p>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <p>Lorem</p>
+        <p>Officia ullamco incididunt tempor sunt occaecat ad deserunt est aute deserunt quis eu dolore mollit.</p>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <p>Lorem</p>
+        <p>Sunt consequat proident in excepteur nostrud Lorem irure cillum eu deserunt excepteur.</p>
+      </div>
+      <div className="question">
+        <h4>Products you offer</h4>
+        <p>Lorem</p>
+        <p>Sunt consequat proident in excepteur nostrud Lorem irure cillum eu deserunt excepteur.</p>
+      </div>
+    </div>
+  );
+};
+
+export default FAQSection;
--- src/Pages/UsAbout.js
+++ src/Pages/UsAbout.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ServicesSection from "../Components/ServicesSection";
 import AboutSection from "../Components/AboutSection";
+import FAQSection from '../Components/FAQSection';
 const UsAbout = () => {
   return (
     <>
     {/* fragment */} 
       <AboutSection />
       <ServicesSection />
+      <FAQSection/>
     </>
   );
 };
